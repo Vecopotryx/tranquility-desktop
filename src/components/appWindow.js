79fd5e1c8e9d01e43095092447acd8effdecd6b1
Vecopotryx <Repsejk@gmail.com>
2020-12-03 19:54:50 +0100

    Added react-rnd for window resizing and dragging

--- src/components/appWindow.js
+++ src/components/appWindow.js
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
 import "./appWindow.css";
+import { Rnd } from "react-rnd";
 
 class AppWindow extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="appWindow">
+        <Rnd
+          className="appWindow"
+          default={{
+            x: 0,
+            y: 0,
+          }}
+        >
           <div className="titlebar">
             <button className="closeWindow">×</button>
             <a className="appName">{this.props.appName}</a>
             <button className="collapseWindow">▲</button>
           </div>
-          <div className="appContent">
-            {this.props.children}
-          </div>
-        </div>
+          <div className="appContent">{this.props.children}</div>
+        </Rnd>
       </React.Fragment>
     );
   }
