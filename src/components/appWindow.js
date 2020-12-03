df2812c481f862a1a5e6c4c88941d755607b0349
Vecopotryx <Repsejk@gmail.com>
2020-12-03 19:01:24 +0100

    Created basic titlebar for appWindow

--- src/components/appWindow.js
+++ src/components/appWindow.js
@@ -1,14 +1,20 @@
-import { render } from '@testing-library/react';
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./appWindow.css";
 
 class AppWindow extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <h1>Stuff</h1>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="appWindow">
+          <div className="titlebar">
+            <button className="closeWindow">×</button>
+            <a className="appName">{this.props.appName}</a>
+            <button className="collapseWindow">▲</button>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default AppWindow;
