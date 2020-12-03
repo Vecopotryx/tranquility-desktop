bd3af00f612bac11a550798bbab61e355fd4a908
Vecopotryx <Repsejk@gmail.com>
2020-12-03 18:23:00 +0100

    Created new component for application windows

---
+++ src/components/appWindow.js
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+
+class AppWindow extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Stuff</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default AppWindow;
