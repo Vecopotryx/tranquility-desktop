14dc0de30d483a7816b6a1ac7018dc0c943b95e4
Vecopotryx <Repsejk@gmail.com>
2020-12-03 18:23:00 +0100

    Created new component for application windows

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import AppWindow from './components/appWindow';
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <AppWindow/>
     </div>
   );
 }
