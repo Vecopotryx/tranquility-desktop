f67067fb13e394d7e3e7c8852881516df26d8515
Vecopotryx <Repsejk@gmail.com>
2020-12-05 15:25:42 +0100

    Added Chip Player JS + Tweaks to window management

--- src/components/appWindow.js
+++ src/components/appWindow.js
@@ -7,10 +7,17 @@ class AppWindow extends Component {
     maxHeight: "3000",
     storedWidth: "100",
     storedHeight: "200",
+    internalFrameOverlayVisible: false,
   };
 
 
   // Todo: Make constructor that sets maxHeight to availHeight directly
+
+  componentDidMount() {
+    this.setState({ storedHeight: this.props.defaultHeight, storedWidth: this.props.defaultWidth, maxHeight: window.screen.availHeight });
+    this.props.onFocus(this.props.id, this.props.zIndex);
+  }
+
   updateCollapse = () => {
     this.rnd.updateSize({width: this.state.storedWidth, height: this.state.appContentCollapsed ?  this.state.storedHeight : "0" });
     this.setState({appContentCollapsed: !this.state.appContentCollapsed, maxHeight: this.state.appContentCollapsed ?  window.screen.availHeight : "100%"})
@@ -20,7 +27,7 @@ class AppWindow extends Component {
     return (
       <React.Fragment>
         <Rnd
-        style={{zIndex: this.props.zIndex}}
+          style={{zIndex: this.props.zIndex, filter: this.props.isUnfocused ? "grayscale(60%)" : "grayscale(0%)"}}
           ref={c => { this.rnd = c; }} 
           className="appWindow"
           maxHeight={this.state.maxHeight}
@@ -29,36 +36,46 @@ class AppWindow extends Component {
             if(!this.state.appContentCollapsed){
               this.setState({
                 storedWidth: ref.style.width,
-                storedHeight: ref.style.height
+                storedHeight: ref.style.height,
+                internalFrameOverlayVisible: false
               });
             } else {
               this.setState({
-                storedWidth: ref.style.width
+                storedWidth: ref.style.width,
+                internalFrameOverlayVisible: false
               });
             }
           }}
           cancel=".collapseWindow, .closeWindow"
           onDragStop={() => {
+            this.setState({internalFrameOverlayVisible: false});
             this.props.onResizeOrDragStop();
           }}
           onResizeStart={() => {
+            this.setState({internalFrameOverlayVisible: true});
             this.props.onResizeOrDragStart();
           }}
           onDragStart={() => {
+            this.setState({internalFrameOverlayVisible: true});
             this.props.onResizeOrDragStart();
           }}
           default={{
             x: 700,
             y: 400,
+            height: this.props.defaultHeight,
+            width: this.props.defaultWidth,
           }}
         >
           <div className="appWrapper" onMouseEnter={() => this.props.onFocus(this.props.id, this.props.zIndex)}>
           <div className="titlebar">
             <button onClick={() => this.props.onClose(this.props.id)} className="closeWindow">×</button>
-            <a className="appName">{this.props.appName}</a>
+            <a className="appName" style={{color: this.props.isUnfocused ? "gray" : "black"}}>{this.props.appName}</a>
             <button onClick={this.updateCollapse} className="collapseWindow">{this.state.appContentCollapsed ? "▼" : "▲"}</button>
           </div>
-          <div className="appContent" style={{display: this.state.appContentCollapsed ? "none" : "block"}}>{this.props.children}</div>
+          <div className="appContent" style={{display: this.state.appContentCollapsed ? "none" : "block", backgroundColor: this.props.backgroundColor}}>
+            <div style={{display: this.state.internalFrameOverlayVisible ? "block" : "none"}} className="internalFrameOverlay"/>
+            {this.props.children}
+            </div>
           </div>
 
         </Rnd>
---
+++ src/components/chipPlayer.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class ChipPlayer extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <iframe
+          title="chipPlayerJS"
+          height="100%"
+          src="https://mmontag.github.io/chip-player-js/browse/Modland/coma's%20favorite%20Fasttracker%202/Elwood?play=Modland%2Fcoma%27s+favorite+Fasttracker+2%2FElwood%2Finto+the+shadow.xm"
+          width="100%"
+          style={{border: "none"}}
+        >
+          <p>Your browser does not support iframes.</p>
+        </iframe>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ChipPlayer;
