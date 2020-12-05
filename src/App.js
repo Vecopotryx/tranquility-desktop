828d4b37d25edd7617ae84552e5672beaa5526b3
Vecopotryx <Repsejk@gmail.com>
2020-12-05 18:35:48 +0100

    Started work on restructuring and  theming

--- src/App.js
+++ src/App.js
@@ -1,102 +1,24 @@
-import React, { Component } from "react";
-import AppWindow from "./components/appWindow";
-import Notes from "./components/notes";
-import ChipPlayer from "./components/chipPlayer";
+import React, { UseState } from "react";
 import Menubar from "./components/menubar";
-import './components/appWindow.css';
+import { useDarkMode } from './styles/useDarkMode'
+import { GlobalStyles, lightTheme, darkTheme } from './styles/globalStyles';
+import { ThemeProvider } from 'styled-components';
+import WindowManager from "./components/WindowManager";
 
-class App extends Component {
-  state = {
-    appWindows: [
-      { id: 1, appName: "Notes", appComponent: <Notes />, zIndex: 1, defaultWidth: 200, defaultHeight: 200, backgroundColor: "white", isUnfocused: false },
-      { id: 2, appName: "Chip Player JS", appComponent: <ChipPlayer />, zIndex: 2, defaultWidth: "50%", defaultHeight: 500, backgroundColor: "#010088", isUnfocused: false  },
-    ],
-    lastFocused: 0,
-    frameOverlayVisible: false,
-    frameOverlayIndex: 2,
-  };
 
-  handleClose = (appId) => {
-    const appWindows = this.state.appWindows.filter((c) => c.id !== appId);
-    this.setState({ appWindows });
-  };
+function App() {
+  const [ theme, toggleTheme ] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
-  handleOpen = (appName, appComponent, defaultWidth, defaultHeight, backgroundColor) => {
-    let newId =
-      Math.max(...this.state.appWindows.map((appWindow) => appWindow.id)) + 1;
-    let highest = Math.max(
-      ...this.state.appWindows.map((appWindow) => appWindow.zIndex)
-    );
-    if (!isFinite(newId)) {
-      newId = 1; // Makes sure that newId is a valid number, as I had problems with it getting set to -Infinity when appWindows was empty.
-    }
-    this.setState((prevState) => ({
-      appWindows: [
-        ...prevState.appWindows,
-        {
-          id: newId,
-          appName: appName,
-          appComponent: appComponent,
-          zIndex: 1,
-          defaultWidth: defaultWidth,
-          defaultHeight: defaultHeight,
-          backgroundColor: backgroundColor
-        },
-      ],
-      frameOverlayIndex: highest + 1,
-      lastFocused: newId - 1,
-    }));
-  };
-
-  handleFocus = (appId, zIndex) => {
-    if (appId !== this.state.lastFocused) {
-      let highest = Math.max(
-        ...this.state.appWindows.map((appWindow) => appWindow.zIndex)
-      );
-      if (zIndex < highest) {
-        const newAppWindows = this.state.appWindows.map((appWindow) => {
-          if (appWindow.id !== appId) return { ...appWindow, isUnfocused: true };
-          return { ...appWindow, zIndex: highest + 1, isUnfocused: false };
-        });
-        this.setState({ appWindows: newAppWindows, lastFocused: appId, frameOverlayIndex: highest + 1});
-      }
-    }
-  };
-
-  handleResizeOrDragStart = () => {
-    this.setState({ frameOverlayVisible: true });
-  }
-
-  handleResizeOrDragStop = () => {
-    this.setState({ frameOverlayVisible: false });
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <Menubar onOpen={this.handleOpen}></Menubar>
-        <div style={{display: this.state.frameOverlayVisible ? "block" : "none", zIndex: this.state.frameOverlayIndex}} className="frameOverlay"/>
-        {this.state.appWindows.map((appWindow) => (
-          <AppWindow
-            key={appWindow.id}
-            id={appWindow.id}
-            appName={appWindow.appName}
-            zIndex={appWindow.zIndex}
-            defaultHeight={appWindow.defaultHeight}
-            defaultWidth={appWindow.defaultWidth}
-            backgroundColor={appWindow.backgroundColor}
-            isUnfocused={appWindow.isUnfocused}
-            onClose={this.handleClose}
-            onFocus={this.handleFocus}
-            onResizeOrDragStart={this.handleResizeOrDragStart}
-            onResizeOrDragStop={this.handleResizeOrDragStop}
-          >
-            {appWindow.appComponent}
-          </AppWindow>
-        ))}
-      </React.Fragment>
-    );
-  }
+  return (
+    <ThemeProvider theme={themeMode}>
+      <div>
+        <GlobalStyles/>
+        <Menubar/>
+        <WindowManager/>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/components/WindowManager.js
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import AppWindow from "./appWindow";
+import Notes from "./notes";
+import ChipPlayer from "./chipPlayer";
+import './appWindow.css';
+import { Settings } from './settings'
+
+class WindowManager extends Component {
+  state = {
+    appWindows: [
+      { id: 1, appName: "Notes", appComponent: <Settings theme={this.props.themeManagementTheme} toggleTheme={this.props.themeManagementToggle}/>, zIndex: 1, defaultWidth: 200, defaultHeight: 200, isUnfocused: false },
+      { id: 2, appName: "Chip Player JS", appComponent: <ChipPlayer />, zIndex: 2, defaultWidth: "50%", defaultHeight: 500, isUnfocused: false  },
+    ],
+    lastFocused: 0,
+    frameOverlayVisible: false,
+    frameOverlayIndex: 2,
+  };
+
+  handleClose = (appId) => {
+    const appWindows = this.state.appWindows.filter((c) => c.id !== appId);
+    this.setState({ appWindows });
+  };
+
+  handleOpen = (appName, appComponent, defaultWidth, defaultHeight, backgroundColor) => {
+    let newId =
+      Math.max(...this.state.appWindows.map((appWindow) => appWindow.id)) + 1;
+    let highest = Math.max(
+      ...this.state.appWindows.map((appWindow) => appWindow.zIndex)
+    );
+    if (!isFinite(newId)) {
+      newId = 1; // Makes sure that newId is a valid number, as I had problems with it getting set to -Infinity when appWindows was empty.
+    }
+    this.setState((prevState) => ({
+      appWindows: [
+        ...prevState.appWindows,
+        {
+          id: newId,
+          appName: appName,
+          appComponent: appComponent,
+          zIndex: 1,
+          defaultWidth: defaultWidth,
+          defaultHeight: defaultHeight,
+          backgroundColor: backgroundColor
+        },
+      ],
+      frameOverlayIndex: highest + 1,
+      lastFocused: newId - 1
+    }));
+  };
+
+  handleFocus = (appId, zIndex) => {
+    if (appId !== this.state.lastFocused) {
+      let highest = Math.max(
+        ...this.state.appWindows.map((appWindow) => appWindow.zIndex)
+      );
+      if (zIndex < highest) {
+        const newAppWindows = this.state.appWindows.map((appWindow) => {
+          if (appWindow.id !== appId) return { ...appWindow, isUnfocused: true };
+          return { ...appWindow, zIndex: highest + 1, isUnfocused: false };
+        });
+        this.setState({ appWindows: newAppWindows, lastFocused: appId, frameOverlayIndex: highest + 1});
+      }
+    }
+  };
+
+  handleResizeOrDragStart = () => {
+    this.setState({ frameOverlayVisible: true });
+  }
+
+  handleResizeOrDragStop = () => {
+    this.setState({ frameOverlayVisible: false });
+  }
+
+  handleSettings = () => {
+    this.setState({ frameOverlayVisible: false });
+  }
+
+  handleInformation = () => {
+
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+
+
+        <div style={{display: this.state.frameOverlayVisible ? "block" : "none", zIndex: this.state.frameOverlayIndex}} className="frameOverlay"/>
+        {this.state.appWindows.map((appWindow) => (
+          <AppWindow
+            key={appWindow.id}
+            id={appWindow.id}
+            appName={appWindow.appName}
+            zIndex={appWindow.zIndex}
+            defaultHeight={appWindow.defaultHeight}
+            defaultWidth={appWindow.defaultWidth}
+            isUnfocused={appWindow.isUnfocused}
+            onClose={this.handleClose}
+            onFocus={this.handleFocus}
+            onResizeOrDragStart={this.handleResizeOrDragStart}
+            onResizeOrDragStop={this.handleResizeOrDragStop}
+            onSettingsApply={this.handleSettings}
+            theme={this.props.theme}
+          >
+            {appWindow.appComponent}
+          </AppWindow>
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+//             backgroundColor={appWindow.backgroundColor}
+//  backgroundColor: "#010088",
+
+export default WindowManager;
