03427d5b92d472e515567462e065aa771b487220
Vecopotryx <Repsejk@gmail.com>
2020-12-06 10:51:19 +0100

    Added the ability to change the background image

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
-import Menubar from "./components/menubar";
-import { GlobalStyles, lightTheme, darkTheme } from './styles/globalStyles';
+import { GlobalStyles, lightTheme, darkTheme, classicTheme } from './styles/globalStyles';
 import { ThemeProvider } from 'styled-components';
 import WindowManager from './components/WindowManager';
 
 function App() {
   const [theme, setTheme] = useState("dark");
-  
+  const [background, setBackground] = useState("url('https://raw.githubusercontent.com/Vecopotryx/retro-desktop-environment/master/source/img/andreas-gucklhorn-IRq79QU9ZGU-unsplash.jpg');");
+
   let themeMode = lightTheme;
   switch(theme){
     case 'dark':
       themeMode = darkTheme;
       break;
     case 'classic':
+      themeMode = classicTheme;
       break;
     case 'platinum':
       break;
@@ -23,9 +24,8 @@ function App() {
   return (
     <ThemeProvider theme={themeMode}>
       <div>
-        <GlobalStyles/>
-        <Menubar/>
-        <WindowManager theme={theme} setTheme={setTheme} />
+        <GlobalStyles background={background} />
+        <WindowManager theme={theme} setTheme={setTheme} background={background} setBackground={setBackground}/>
       </div>
     </ThemeProvider>
   );
--- src/components/settings.js
+++ src/components/settings.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-export const Settings = ({ theme, setTheme }) => {
+export const Settings = ({ theme, setTheme, background, setBackground }) => {
   return (
     <React.Fragment>
+      <h1>{background}</h1>
+      <button onClick={() => setBackground("url('https://images.unsplash.com/photo-1607166303098-19ed940446bd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80');")}>Change background</button>
       <button onClick={() => setTheme("light")}>Light</button>
       <button onClick={() => setTheme("dark")}>Dark</button>
+      <button onClick={() => setTheme("classic")}>Classic</button>
     </React.Fragment>
   );
 };
