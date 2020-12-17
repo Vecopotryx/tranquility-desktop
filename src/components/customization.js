085c4fb0b0de56b6a806dac3680b91dfcda21ccd
Vecopotryx <Repsejk@gmail.com>
2020-12-17 20:16:36 +0100

    Converted customization to functional component

--- src/components/customization.js
+++ src/components/customization.js
@@ -1,80 +1,102 @@
-import React, { Component } from "react";
-import darkmodeImage from './darkmode-temporary.svg';
-import defaultBackground1 from '../img/andreas-gucklhorn-IRq79QU9ZGU-unsplash.jpg';
-import defaultBackground2 from '../img/KDE Plasma Scenery 64 NO LOGO.jpg';
-import defaultBackground3 from '../img/nasa-Q1p7bh3SHj8-unsplash.jpg';
+import React, { useState } from "react";
+import darkmodeImage from "./darkmode-temporary.svg";
+import defaultBackground1 from "../img/andreas-gucklhorn-IRq79QU9ZGU-unsplash.jpg";
+import defaultBackground2 from "../img/KDE Plasma Scenery 64 NO LOGO.jpg";
+import defaultBackground3 from "../img/nasa-Q1p7bh3SHj8-unsplash.jpg";
 
+export default function Customization(props) {
+  const [unsplashTerm, setUnsplashTerm] = useState("");
+  const [currentBackground, setCurrentBackground] = useState(props.background);
+  const [currentTheme, setCurrentTheme] = useState(props.theme);
 
-class Customization extends Component {
-  state = {
-    unsplashTerm: "",
-  };
+  // - Need to allow user to click image to select theme.
+  // - Perhaps return object with other settings then parse them in the future?
 
-  onImageChange = (event) => {
+  const onImageChange = (event) => {
     if (event.target.files && event.target.files[0]) {
-      let newImage =  URL.createObjectURL(event.target.files[0]);
-      this.setState({
-        image: newImage,
-      });
-      this.props.setBackground("url(" + newImage + ") ");
+      let newImage = URL.createObjectURL(event.target.files[0]);
+      setCurrentBackground(newImage);
+      props.setBackground("url(" + newImage + ") ");
     }
   };
 
-  componentDidMount() {
-    this.setState({ image: this.props.background.replace("url('",'').replace("')",''), themeOption: this.props.theme});
-  }
-
-  themeHandler = (changeEvent) => {
-    this.setState({
-      themeOption: changeEvent.target.value,
-    });
-    this.props.setTheme(changeEvent.target.value);
+  const themeHandler = (changeEvent) => {
+    setCurrentTheme(changeEvent.target.value);
+    props.setTheme(changeEvent.target.value);
   };
 
-  unsplashHandler = () => {
-    let widthHeight = window.screen.availWidth + "x" + window.screen.availHeight;
+  const unsplashHandler = () => {
+    let widthHeight =
+      window.screen.availWidth + "x" + window.screen.availHeight;
     let d = new Date();
-    let newImage = "https://source.unsplash.com/" + widthHeight + "/?" + this.state.unsplashTerm + "/" + d.getTime()
-    this.setState({
-        image: newImage,
-      });
-    this.props.setBackground("url(" + newImage + ") ");
-  }
-  
-  handleUnsplashInput = (changeEvent) => {
-    this.setState({
-      unsplashTerm: changeEvent.target.value,
-    });
+    let newImage =
+      "https://source.unsplash.com/" +
+      widthHeight +
+      "/?" +
+      unsplashTerm +
+      "/" +
+      d.getTime();
+    setCurrentBackground(newImage);
+    props.setBackground("url(" + newImage + ") ");
   };
 
+  const handleUnsplashInput = (changeEvent) => {
+    setUnsplashTerm(changeEvent.target.value);
+  };
 
-  // Need to prevent user from opening several settings windows.
-  // Also need to allow user to click image to select theme
-  // Return array with settings then parse them?
-  render() {
-    return (
-      <div className="customization">
-        <div className="themePicker">
-            <h2>Theme</h2>
-            <div className="settingsPreviewsHolder">
-              <div className="settingsPreviews" style={{backgroundImage: "url(" + this.state.image + ")", marginRight: "1%"}}>
-                <img className="settingsThemePreview" src={darkmodeImage} width="100%" alt=""></img>
-              </div>
-              <div className="settingsPreviews" style={{backgroundImage: "url(" + this.state.image + ")"}}>
-                <img className="settingsThemePreview" src={darkmodeImage} width="100%" alt=""></img>
-              </div>
-              <div className="settingsPreviews" style={{backgroundImage: "url(" + this.state.image + ")", marginLeft: "1%"}}>
-                <img className="settingsThemePreview" src={darkmodeImage} width="100%" alt=""></img>
-              </div>
-            </div>
+  return (
+    <div className="customization">
+      <div className="themePicker">
+        <h2>Theme</h2>
+        <div className="settingsPreviewsHolder">
+          <div
+            className="settingsPreviews"
+            style={{
+              backgroundImage: "url(" + currentBackground + ")",
+              marginRight: "1%",
+            }}
+          >
+            <img
+              className="settingsThemePreview"
+              src={darkmodeImage}
+              width="100%"
+              alt=""
+            ></img>
+          </div>
+          <div
+            className="settingsPreviews"
+            style={{ backgroundImage: "url(" + currentBackground + ")" }}
+          >
+            <img
+              className="settingsThemePreview"
+              src={darkmodeImage}
+              width="100%"
+              alt=""
+            ></img>
+          </div>
+          <div
+            className="settingsPreviews"
+            style={{
+              backgroundImage: "url(" + currentBackground + ")",
+              marginLeft: "1%",
+            }}
+          >
+            <img
+              className="settingsThemePreview"
+              src={darkmodeImage}
+              width="100%"
+              alt=""
+            ></img>
+          </div>
+        </div>
 
-          <div className="settingsThemeRadiosHolder">
+        <div className="settingsThemeRadiosHolder">
           <label className="settingsThemeRadios">
             <input
               type="radio"
               value="light"
-              checked={this.state.themeOption === "light"}
-              onChange={this.themeHandler}
+              checked={currentTheme === "light"}
+              onChange={themeHandler}
             />
             Light
           </label>
@@ -82,8 +104,8 @@ class Customization extends Component {
             <input
               type="radio"
               value="dark"
-              checked={this.state.themeOption === "dark"}
-              onChange={this.themeHandler}
+              checked={currentTheme === "dark"}
+              onChange={themeHandler}
             />
             Dark
           </label>
@@ -91,54 +113,64 @@ class Customization extends Component {
             <input
               type="radio"
               value="classic"
-              checked={this.state.themeOption === "classic"}
-              onChange={this.themeHandler}
+              checked={currentTheme === "classic"}
+              onChange={themeHandler}
             />
             Classic
           </label>
-          </div>
-        </div>
-        <div className="backgroundPicker">
-          <h2>Background</h2>
-          <img className="settingsCurrentBackgroundPreview" src={this.state.image} width="100%" alt=""></img>
-          <hr width="99%"/>
-          <div className="settingsPreviewsHolder">
-            <img className="settingsBackgroundPreview" src={defaultBackground1} style={{marginRight: "1%"}} width="100%" alt=""></img>
-            <img className="settingsBackgroundPreview" src={defaultBackground2} width="100%" alt=""></img>
-            <img className="settingsBackgroundPreview" src={defaultBackground3} style={{marginLeft: "1%"}} width="100%" alt=""></img>
-
-          </div>
-          <div className="settingsBackgroundOptions">
-            <h3>Set background</h3>
-            <label>
-              Random from Unsplash
-              <br/>
-              <input type="text" onChange={this.handleUnsplashInput}></input>
-              <button onClick={this.unsplashHandler}>Search for photo</button>
-            </label>
-
-            <br/>
-            <label>
-              Upload from computer
-              <br/>
-
-              <input
-                type="file"
-                onChange={this.onImageChange}
-              />
-            </label>
-
-          </div>
-
         </div>
       </div>
-    );
-  }
+      <div className="backgroundPicker">
+        <h2>Background</h2>
+        <img
+          className="settingsCurrentBackgroundPreview"
+          src={currentBackground}
+          width="100%"
+          alt=""
+        ></img>
+        <hr width="99%" />
+        <div className="settingsPreviewsHolder">
+          <img
+            className="settingsBackgroundPreview"
+            src={defaultBackground1}
+            style={{ marginRight: "1%" }}
+            width="100%"
+            alt=""
+          ></img>
+          <img
+            className="settingsBackgroundPreview"
+            src={defaultBackground2}
+            width="100%"
+            alt=""
+          ></img>
+          <img
+            className="settingsBackgroundPreview"
+            src={defaultBackground3}
+            style={{ marginLeft: "1%" }}
+            width="100%"
+            alt=""
+          ></img>
+        </div>
+        <div className="settingsBackgroundOptions">
+          <h3>Set background</h3>
+          <label>
+            Random from Unsplash
+            <br />
+            <input type="text" onChange={handleUnsplashInput}></input>
+            <button onClick={unsplashHandler}>Search for photo</button>
+          </label>
+
+          <br />
+          <label>
+            Upload from computer
+            <br />
+            <input type="file" onChange={onImageChange} />
+          </label>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Customization;
-
-
 
 /* Currently having problems getting this to work, as it for some reason doesn't want to accept these properties in the "background: " property,
 so I'm keeping this down here until I get it working.
@@ -231,8 +263,7 @@ so I'm keeping this down here until I get it working.
         </div>
         */
 
-
-        /*
+/*
         Also storing this for later as, while it was working, I'm hoping to implement a better/cleaner solution
           handleBackgroundColor = (changeEvent) => {
     this.setState({
