f86ba2a76add417663b3853709f4cca74e3e7544
Vecopotryx <Repsejk@gmail.com>
2021-01-04 20:37:52 +0100

    Rewrote Menubar + re-added ability to open windows

---
+++ src/components/Menubar.tsx
@@ -0,0 +1,119 @@
+import React, { useEffect } from "react";
+import "./menubar.css";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Notes from "./Notes";
+import ChipPlayer from "./ChipPlayer";
+import Settings from "./Settings";
+import appIconPlaceholder from "../img/andreas-gucklhorn-IRq79QU9ZGU-unsplash.jpg";
+
+type MenubarProps = {
+  handleOpen: (title: string, component: JSX.Element) => void;
+}
+
+const Menubar = ({handleOpen}: MenubarProps) =>  {
+  const AppButtons = [
+    {
+      appName: "Notes",
+      appComponent: <Notes />,
+      buttonText: "Notes",
+      defaultWidth: 200,
+      defaultHeight: 200,
+      appIcon: appIconPlaceholder,
+    },
+    {
+      appName: "Chip Player JS",
+      appComponent: <ChipPlayer />,
+      buttonText: "Chip Player",
+      defaultWidth: "50%",
+      defaultHeight: 500,
+      customBackground: "#010088",
+      appIcon: appIconPlaceholder,
+    },
+  ];
+
+  useEffect(() => {
+    setInterval(() => {
+       time = new Date();
+    }, 1000);
+  }, [])
+
+  let time = new Date();
+
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  /*
+                          appButton.defaultWidth,
+                        appButton.defaultHeight,
+                        appButton.appIcon,
+                        appButton.customBackground*/
+  return (
+    <>
+      <nav className="menubar">
+        <div className="menubarLeft">
+          <NavDropdown id="applications" className="menubarDropdown" title="Applications">
+            <div className="menubarList">
+              {AppButtons.map((appButton) => (
+                <NavDropdown.Item>
+                  <button
+                    className="menubarButton"
+                    onClick={() =>
+                      handleOpen(
+                        appButton.appName,
+                        appButton.appComponent,
+                      )
+                    }
+                  >
+                    <img src={appButton.appIcon} alt=""></img>
+                    <a style={{ float: "left" }}>{appButton.buttonText}</a>
+                  </button>
+                  <NavDropdown.Divider />
+                </NavDropdown.Item>
+              ))}
+            </div>
+          </NavDropdown>
+
+          <NavDropdown id="options" className="menubarDropdown" title="Options">
+            <div className="menubarList">
+              <NavDropdown.Item>
+                <button
+                  className="menubarButton"
+                  onClick={() =>
+                    handleOpen(
+                      "Settings",
+                      <Settings/>,
+                    )
+                  }
+                >
+                  Settings
+                </button>
+                <NavDropdown.Divider />
+              </NavDropdown.Item>
+            </div>
+          </NavDropdown>
+        </div>
+        <div className="menubarRight">
+          <a>
+            {days[time.getDay()]}{" "}
+            {time.toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+//           <span className="openwindowListStuff">{this.props.children}</span>
+
+
+export default Menubar;
--- src/components/WindowManager.tsx
+++ src/components/WindowManager.tsx
@@ -1,28 +1,54 @@
-import React from "react";
-import Notes from "./Notes";
+import { Console } from "console";
+import React, { SetStateAction, Dispatch, FC } from "react";
 import AppWindow from "./AppWindow";
+import Menubar from "./Menubar";
+import ChipPlayer from "./ChipPlayer";
+
+
+interface WindowListProps {
+  id?: number;
+  title?: string;
+  component?: React.FC;
+  index?: number;
+  isFocused?: boolean;
+}
+
+/*
+interface WindowManagerProps {
+  windowList: [WindowListProps];
+  setWindowList: any;
+  // setWindowList: Dispatch<SetStateAction<WindowListProps>>;
+}*/
 
 const WindowManager = () => {
+  const [status, setStatus] = React.useState();
+
   const [windowList, setWindowList] = React.useState([
     {
       id: 1,
       title: "Test",
-      component: <Notes />,
+      component: <ChipPlayer />,
       index: 1,
       isFocused: false,
     },
     {
       id: 2,
       title: "Test2",
-      component: <Notes />,
+      component: <ChipPlayer />,
       index: 2,
       isFocused: true,
     },
   ]);
-  const [status, setStatus] = React.useState();
+
+  const handleOpen = (title: string, component: JSX.Element) => {
+    console.log(title, component);
+    const newArr = [...windowList, {id: 1, title: title, component: component, index: 1, isFocused: true}]
+    setWindowList(newArr);
+  }
 
   return (
     <>
+      <Menubar handleOpen={handleOpen}/>
       <div className="WindowContainer">
         {windowList.map((appWindow) => (
           <AppWindow
@@ -30,7 +56,7 @@ const WindowManager = () => {
             title={appWindow.title}
             index={appWindow.index}
           >
-            <h2>Test</h2>
+            {appWindow.component}
           </AppWindow>
         ))}
       </div>
