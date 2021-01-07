832473be5694b899ec0ec59f6a024b0dd16d84f0
Vecopotryx <Repsejk@gmail.com>
2021-01-07 16:48:13 +0100

    Fixed a few issues in Getpost Gavinator

--- src/applications/Browser.tsx
+++ src/applications/Browser.tsx
@@ -2,33 +2,28 @@ import React, { useState } from "react";
 import loadingSpinner from "../assets/img/tail-spin.svg";
 
 const Browser = () => {
-  let year = 2000;
-  let site = "http://w3.org";
-
-  // const [year, setYear] = useState(2000);
-  // const [site, setSite] = useState("https://www.w3.org");
+  const [year, setYear] = useState(2000);
+  const [site, setSite] = useState("https://www.w3.org");
   const [loading, setLoading] = useState(true);
   const [frameSource, setFrameSource] = useState(
-    "https://web.archive.org/web/2000/0301012010/http://w3.org"
+    "https://web.archive.org/web/20000301012010/http://w3.org"
   );
 
   const updateIframe = () => {
     const newSource =
       "https://web.archive.org/web/" + year + "0301012010/" + site;
-    console.log(newSource);
-    console.log(frameSource);
-    if (newSource !== frameSource) {
-      setLoading(true);
-      setFrameSource(newSource);
+    setFrameSource(newSource);
+    if(frameSource !== newSource){
+        setLoading(true);
     }
   };
 
   return (
-    <div className="browser">
-      <div id="browserNavbar">
+    <div className="browser" style={{ height: "100%" }}>
+      <div id="browserNavbar" style={{ textAlign: "center" }}>
         <label>
           Year:
-          <select onChange={(e) => (year = Number(e.target.value))}>
+          <select onChange={(e) => setYear(Number(e.target.value))}>
             <option value="2000">2000</option>
             <option value="2005">2005</option>
             <option value="2010">2010</option>
@@ -38,12 +33,16 @@ const Browser = () => {
         <input
           type="text"
           placeholder="Enter address"
-          onChange={(e) => (site = e.target.value)}
+          onChange={(e) => setSite(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === "Enter") updateIframe();
+          }}
+          style={{ marginLeft: "1%", marginRight: "1%", width: "50%" }}
         />
         <button onClick={() => updateIframe()}>Go</button>
         {loading && (
           <>
-            <img className="loadingSpinner" src={loadingSpinner}></img>
+            <img className="loadingSpinner" src={loadingSpinner} alt="Loading"></img>
             <p>
               Retrieving data from
               <a href="https://archive.org/">Archive.org</a>
@@ -55,7 +54,7 @@ const Browser = () => {
         title="browserFrame"
         src={frameSource}
         width="100%"
-        height="300%"
+        height="100%"
         onLoad={() => setLoading(false)}
       >
         <p>Your browser does not support iframes.</p>
