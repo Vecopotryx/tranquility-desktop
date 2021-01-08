f6aa785b933fc729460c4a6f184d8e34c909c3d6
Vecopotryx <Repsejk@gmail.com>
2021-01-08 15:42:37 +0100

    Started working on about screen

--- src/applications/settings/Settings.tsx
+++ src/applications/settings/Settings.tsx
@@ -1,35 +1,47 @@
-import React, { ReactElement, useState } from 'react'
-import Customization from './Customization'
-import BackgroundPicker from './BackgroundPicker'
-import { useSettings } from '../../contexts/SettingsContext'
+import React, { ReactElement, useState } from "react";
+import Customization from "./Customization";
+import BackgroundPicker from "./BackgroundPicker";
+import About from "./About";
+import { useSettings } from "../../contexts/SettingsContext";
 
 const Settings = () => {
-    const [openSettings, setOpenSettings] = useState("customization");
+  const [openSettings, setOpenSettings] = useState("customization");
 
-    const settings = useSettings().customizeSettings;
-    const setSettings = useSettings().setCustomizeSettings;
+  const settings = useSettings().customizeSettings;
+  const setSettings = useSettings().setCustomizeSettings;
 
-    const OpenedSettings = (): ReactElement => {
-        switch(openSettings) {
-            case "customization":
-                return <Customization settings={settings} setSettings={setSettings}/>
-            case "background":
-                return <BackgroundPicker settings={settings} setSettings={setSettings}/>
-            default:
-                break;
-        }
-        return <h1>Error</h1>
-      }; 
+  const OpenedSettings = (): ReactElement => {
+    switch (openSettings) {
+      case "customization":
+        return <Customization settings={settings} setSettings={setSettings} />;
+      case "background":
+        return (
+          <BackgroundPicker settings={settings} setSettings={setSettings} />
+        );
+      case "about":
+        return (
+          <About />
+        );
+      default:
+        break;
+    }
+    return <h1>Error</h1>;
+  };
 
-    return (
-        <div>
-            <div>
-                <button onClick={() => setOpenSettings("customization")}>Customization</button>
-                <button onClick={() => setOpenSettings("background")}>Background</button>
-            </div>
-            <OpenedSettings/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>
+        <button onClick={() => setOpenSettings("customization")}>
+          Customization
+        </button>
+        <button onClick={() => setOpenSettings("background")}>
+          Background
+        </button>
+        <button onClick={() => setOpenSettings("about")}>About</button>
+      </div>
+      <OpenedSettings />
+    </div>
+  );
+};
 
 export default Settings;
