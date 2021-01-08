683989e5c651ecaecbf5489a4dd41e655a885d58
Vecopotryx <Repsejk@gmail.com>
2021-01-08 14:58:30 +0100

    Added "neofetch" / system info command

--- src/applications/Terminal.tsx
+++ src/applications/Terminal.tsx
@@ -35,39 +35,74 @@ const Terminal = () => {
         handleCommand(<Test />);
         break;
       case "clear":
-        clear();
+        setCommandHistory([]);
+        break;
+      case "neofetch":
+        handleCommand(<Neofetch />);
         break;
       default:
-        handleCommand(<InvalidCommand/>);
+        handleCommand(<InvalidCommand />);
         break;
     }
   };
 
   const handleCommand = (element: JSX.Element) => {
-    setCommandHistory([...commandHistory, <PrevUserInput/>, <>{element} <br /></>]);
+    setCommandHistory([
+      ...commandHistory,
+      <PrevUserInput />,
+      <>
+        {element} <br />
+      </>,
+    ]);
   };
 
   const PrevUserInput = () => {
-    return (<p>$ {currentInput}</p>)
-  }
+    return <p>$ {currentInput}</p>;
+  };
 
   const Test = () => {
     return <>Test</>;
   };
 
-  const clear = () => {
-    setCommandHistory([]);
-  }
+  const Neofetch = () => {
+    return (
+      <span style={{ display: "block" }}>
+        <pre>{neofetchContent}</pre>
+      </span>
+    );
+  };
+
+  const neofetchContent = `
+   ____________________ 
+  |  |              |  |      user@RDE
+  |[]|              |[]|      --------
+  |  |              |  |      OS: Retro Desktop Enviornment
+  |  |              |  |      Host: Waygate Miga ST 740
+  |  |              |  |      Kernel: 2.0.0
+  |  |______________|  |      Packages: 8 (rpt)
+  |                    |      Shell: /system/rdsh
+  |     ____________   |      DE: RDE
+  |    | __      |  |  |      WM: RWM
+  |    ||  |     |  |  |      CPU: Interola 68040+ (1) @ 230 MHz
+  |    ||__|     |  |  |      GPU: 2dfx Vodude 16
+  |____|_________|__|__|      Memory: 5 MiB / 256 MiB
+`;
 
   const InvalidCommand = () => {
-    return (<p><i>{currentInput}</i> is not a valid command</p>)
-  }
+    return (
+      <p>
+        <i>{currentInput}</i> is not a valid command
+      </p>
+    );
+  };
 
   return (
     <div>
-      {commandHistory.map((element, index) => <div key={index}>{element}</div>)}
+      {commandHistory.map((element, index) => (
+        <div key={index}>{element}</div>
+      ))}
 
-      <InputLine key={-1} />
+      <InputLine />
     </div>
   );
 };
