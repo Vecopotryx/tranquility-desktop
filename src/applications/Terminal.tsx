0e14e1f48e6c27598a4a20055ecaf0f8fd60c8e6
Vecopotryx <Repsejk@gmail.com>
2021-01-08 13:49:30 +0100

    Testing way to show previous commands in terminal

--- src/applications/Terminal.tsx
+++ src/applications/Terminal.tsx
@@ -4,7 +4,7 @@ const Terminal = () => {
   // const [currentInput, setCurrentInput] = useState<string>("");
 
   let currentInput = "";
-  const InputLine = () => {
+  const InputLine = ({ id }: { id: number }) => {
     return (
       <div>
         $
@@ -21,24 +21,50 @@ const Terminal = () => {
     );
   };
 
-  const [commandHistory, setCommandHistory] = useState<JSX.Element[]>([
-    <InputLine />,
+  interface CommandHistoryProps {
+    id: number;
+    component: JSX.Element;
+  }
+
+  // const [currentId, setCurrentId] = useState(0);
+  // let currentId = 0;
+
+  const [commandHistory, setCommandHistory] = useState<CommandHistoryProps[]>([
+    {
+      id: 0,
+      component: <InputLine id={0} />,
+    },
   ]);
 
   const handleInput = () => {
     switch (currentInput.toLowerCase()) {
       case "test":
-        setCommandHistory([...commandHistory, <Test />]);
+        handleCommand(<Test />);
         break;
       default:
+        handleCommand(<p>{currentInput}</p>)
         break;
     }
+  };
+
+  const handleCommand = (element: JSX.Element) => {
+    setCommandHistory([
+      ...commandHistory,
+      { id: 1, component: element },
+      { id: 1, component: <br/> },
+    ]);
   };
 
   const Test = () => {
     return <>Test</>;
   };
-  return <div>{commandHistory.map((element) => element)}</div>;
+  return (
+    <div>
+      {commandHistory.map((element) => element.component)}
+
+      <InputLine id={1}/>
+    </div>
+  );
 };
 
 export default Terminal;
