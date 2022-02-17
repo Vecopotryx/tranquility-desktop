1effff4e5e262842a11f474f54d1078a54dfe185
Vecopotryx <repsejk@gmail.com>
2022-02-17 21:01:55 +0100

    Improved Greeter experience

--- src/applications/Greeter.tsx
+++ src/applications/Greeter.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import "../assets/styles/componentStyles/Greeter.css";
 import Logo from "../assets/img/LogoSideView.png";
-import Settings from "./settings/Settings";
+import darkPreview from "../assets/img/preview-dark.svg";
+import lightPreview from "../assets/img/preview-light.svg";
+import classicPreview from "../assets/img/preview-classic.svg";
 import { useSettings } from "../contexts/SettingsContext";
+import defaultBackground3 from "../assets/img/backgrounds/edmond-lau-jba0W6Nssjg-unsplash.jpg";
+import BackgroundPicker from "./settings/BackgroundPicker";
 
 const Greeter = (props: any) => {
   const [currentScreen, setCurrentScreen] = useState("greeting");
@@ -17,49 +21,143 @@ const Greeter = (props: any) => {
         <div className="welcomeText">
           <h1>Welcome to Retro Desktop Environment</h1>
           <p>
-            Configure themes and other settings by pressing the configure button
+            Configure themes and other settings by pressing the forward button, or press the back button to use default settings.
           </p>
         </div>
         <div className="buttonHolder">
-          <button
+          <h1
             className="backButton"
-            onClick={() => {
-              let newSettings = { ...settings, usingLocalStorage: false };
-              setSettings(newSettings);
-              props.handleClose(props.id);
-            }}
+            onClick={() => props.handleClose(props.id)}
           >
-            Continue with default settings
-          </button>
-          <button
+            &#11164;
+          </h1>
+          <h1
             className="forwardButton"
-            onClick={() => setCurrentScreen("settings")}
+            onClick={() => setCurrentScreen("theme")}
           >
-            Configure
-          </button>
+            &#10148;
+          </h1>
         </div>
       </div>
     );
   };
 
-  const GreeterSettings = () => {
+  const updateSettings = (
+    property: string,
+    value: string | boolean | number
+  ) => {
+    let newSettings = { ...settings, [property]: value };
+    setSettings(newSettings);
+    if (newSettings.usingLocalStorage) {
+      localStorage.setItem(
+        "settings",
+        JSON.stringify({
+          ...newSettings,
+          background:
+            "url(" + defaultBackground3 + ")",
+        })
+      );
+    }
+  };
+
+  // TODO: Should perhaps try to import this functionality from Settings instead.
+  interface Props {
+    theme: string;
+    image: string;
+  }
+
+  const ThemePreview = ({ theme, image }: Props) => {
     return (
       <div>
-        <Settings></Settings>
-        <div className="buttonHolder">
-          <button
-            className="backButton"
-            onClick={() => setCurrentScreen("greeting")}
-          >
-            Go back
-          </button>
-          <button
-            className="forwardButton"
-            onClick={() => setCurrentScreen("complete")}
-          >
-            Continue
-          </button>
+        <div
+          className="settingsPreviews"
+          style={{ backgroundImage: settings.background }}
+          onClick={() => updateSettings("theme", theme.toLowerCase())}
+        >
+          <img
+            className="settingsThemePreview"
+            src={image}
+            width="100%"
+            alt={theme.toLowerCase()}
+          ></img>
         </div>
+        <label className="settingsThemeRadios">
+          <input
+            type="radio"
+            value={theme.toLowerCase()}
+            checked={settings.theme === theme.toLowerCase()}
+            onChange={(e) => updateSettings("theme", e.target.value)}
+          />
+          {theme}
+        </label>
+      </div>
+    );
+  };
+
+  interface ButtonProps {
+    previous: string;
+    next: string;
+  }
+
+  const ForwardBackButtons = ({ previous, next }: ButtonProps) => {
+    return (
+      <div className="buttonHolder">
+        <h1
+          className="backButton"
+          onClick={() => setCurrentScreen(previous)}
+        >
+          &#11164;
+        </h1>
+        <h1
+          className="forwardButton"
+          onClick={() => setCurrentScreen(next)}
+        >
+          &#10148;
+        </h1>
+      </div>
+    )
+  }
+
+  const GreeterTheme = () => {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h1>Lets pick a theme</h1>
+        <div style={{ padding: "1em" }} className={"themePreviews"}>
+          <ThemePreview theme={"Light"} image={lightPreview} />
+          <ThemePreview theme={"Dark"} image={darkPreview} />
+          <ThemePreview theme={"Classic"} image={classicPreview} />
+        </div>
+        <p>Font:</p>
+        <label style={{ paddingRight: "0.5em" }}>
+          <input
+            type="radio"
+            value="modern"
+            checked={settings.font === "modern"}
+            onChange={(e) => updateSettings("font", e.target.value)}
+          />
+          <p style={{ fontFamily: "Sans-serif" }}>Modern</p>
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="retro"
+            checked={settings.font === "retro"}
+            onChange={(e) => updateSettings("font", e.target.value)}
+          />
+          <p style={{ fontFamily: "retro" }}>Retro</p>
+        </label>
+        <ForwardBackButtons previous="greeting" next="background" />
+      </div>
+    );
+  };
+
+  const GreeterBackground = () => {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h1>Lets pick a background</h1>
+        <BackgroundPicker settings={settings} setSettings={setSettings} />
+
+        <ForwardBackButtons previous="theme" next="complete" />
       </div>
     );
   };
@@ -67,28 +165,26 @@ const Greeter = (props: any) => {
   const Complete = () => {
     return (
       <div>
-        <div style={{ marginLeft: "1%" }}>
+        <div style={{ marginLeft: "1%", textAlign: "center" }}>
           <h1>Setup complete</h1>
-          <h4>You can change these settings later using the settings app</h4>
+          <h4>You can change these settings (and more) later using the settings app.</h4>
         </div>
         <div className="buttonHolder">
-          <button
+          <h1
             className="backButton"
-            onClick={() => setCurrentScreen("settings")}
+            onClick={() => setCurrentScreen("background")}
           >
-            Go back
-          </button>
-          <button
+            &#11164;
+          </h1>
+          <h1
             className="forwardButton"
             onClick={() => {
-              if (settings.usingLocalStorage) {
-                localStorage.setItem("greeted", "true")
-              }
+              localStorage.setItem("greeted", "true");
               props.handleClose(props.id);
             }}
           >
-            Start using RDE
-          </button>
+            Start using RDE &#10148;
+          </h1>
         </div>
       </div>
     );
@@ -98,8 +194,10 @@ const Greeter = (props: any) => {
     switch (currentScreen) {
       case "greeting":
         return <Greeting />;
-      case "settings":
-        return <GreeterSettings />;
+      case "theme":
+        return <GreeterTheme />;
+      case "background":
+        return <GreeterBackground />;
       case "complete":
         return <Complete />;
       default:
