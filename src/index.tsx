a0214ec9f12fe7e168bd9179068ddcab80cde876
Jesper <repsejk@gmail.com>
2022-05-04 13:03:43 +0200

    Switch ReactDOM.render to createRoot for React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { SettingsProvider } from './contexts/SettingsContext'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <SettingsProvider>
-    <App />
+      <App />
     </SettingsProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
