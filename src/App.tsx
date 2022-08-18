badbdedbfc57c66581a87846ebb5a7c63e46932d
Vecopotryx <repsejk@gmail.com>
2022-08-18 10:27:01 +0200

    Load previous theme using localStorage again

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,21 @@ import "./assets/styles/StaticStyles.css";
 
 function App() {
   const settings = useSettings().customizeSettings;
-  const setSettings = useSettings().setCustomizeSettings;
+  //const setSettings = useSettings().setCustomizeSettings;
 
   useEffect(() => {
-    const storedSettings = localStorage.getItem("settings");
-    if (storedSettings !== null) {
+    /*const storedSettings = localStorage.getItem("settings");
+    if (storedSettings) {
       setSettings(JSON.parse(storedSettings));
-    }
-  }, [setSettings]);
+    }*/
+
+    const theme = localStorage.getItem("theme");
+    const opacity = localStorage.getItem("opacity");
+    const font = localStorage.getItem("font");
+    if (theme) { document.documentElement.setAttribute("data-theme", theme); }
+    if (opacity) { document.documentElement.style.setProperty('--bgopacity', opacity); }
+    if (font) { document.documentElement.style.setProperty('--font', font === "modern" ? "" : font) }
+  }, []);
 
   return (
     <>
