1cc8f9ef57b10014dc9ecb854857cd05d404d992
Vecopotryx <repsejk@gmail.com>
2023-01-17 10:10:02 +0100

    Do not add hash to built asset names

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/tranquility-desktop',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
