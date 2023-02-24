10067cce1a93f2bb45d6d8f1554f2ffb37307f68
Vecopotryx <repsejk@gmail.com>
2023-02-24 18:44:30 +0100

    Set default theme based on time of day

--- src/services/settings-loader.ts
+++ src/services/settings-loader.ts
@@ -3,7 +3,13 @@ export const loadTheming = () => {
     const opacity = localStorage.getItem("opacity");
     const font = localStorage.getItem("font");
     const backgroundImg = localStorage.getItem("backgroundImg");
-    if (theme) { document.documentElement.setAttribute("data-theme", theme); }
+    if (theme) { 
+        document.documentElement.setAttribute("data-theme", theme); 
+    } else {
+        const hours = new Date().getHours();
+        const isDayTime = hours >= 8 && hours < 18;
+        document.documentElement.setAttribute("data-theme", isDayTime ? "light" : "dark");
+    }
     if (opacity) { document.documentElement.style.setProperty('--bgopacity', opacity); }
     if (font) { document.documentElement.style.setProperty('--font', font === "modern" ? "" : font); }
     if (backgroundImg) {
