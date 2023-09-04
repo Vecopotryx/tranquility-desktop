fa56357239ef959382ecaf2f1bc00bb380a3f985
Jesper <contact@vecopotryx.com>
2023-09-04 08:24:01 +0200

    Add support for setting panel position

--- src/panels/Panel.tsx
+++ src/panels/Panel.tsx
@@ -10,7 +10,8 @@ interface PanelProps {
 	widgets: Widget[];
 }
 
-const PanelDiv = styled.div`
+const PanelDiv = styled.div<{ $position: string }>`
+	${(p) => p.$position}: 0;
 	position: absolute;
 	display: grid;
 	grid-template-columns: auto auto 1fr auto;
@@ -23,11 +24,24 @@ const PanelDiv = styled.div`
 	backdrop-filter: blur(10px);
 	background-color: rgba(var(--primary-bg), var(--bgopacity));
 	user-select: none;
+
+	${(p) =>
+		(p.$position === 'right' || p.$position === 'left') &&
+		`
+	grid-template-columns: 1fr;
+	overflow-x: hidden;
+	height: calc(100% - 2em);
+	width: 0.9cm;
+	margin: 0.5em;
+	row-gap: 0.3em;
+	padding: 0.3em 0;
+	line-height: 0.9cm;
+  `}
 `;
 
 export const Panel = ({ position, widgets }: PanelProps) => {
 	return (
-		<PanelDiv>
+		<PanelDiv $position={position}>
 			{widgets.map((widget, index) => (
 				<div key={index}>{widget.component}</div>
 			))}
