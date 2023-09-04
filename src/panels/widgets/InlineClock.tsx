dbe02ce24573f6af0b1c097919449f56ccdd3172
Jesper <contact@vecopotryx.com>
2023-09-04 19:54:49 +0200

    Add back expanded clock view in InlineClock

--- src/panels/widgets/InlineClock.tsx
+++ src/panels/widgets/InlineClock.tsx
@@ -1,4 +1,15 @@
 import { useEffect, useState } from 'react';
+import Dropdown from '../Dropdown';
+import styled from 'styled-components';
+
+const ExpandedView = styled.div`
+	min-width: 4cm;
+	background-color: rgba(var(--primary-bg), var(--bgopacity));
+	border-radius: var(--borderRadius);
+	backdrop-filter: blur(10px);
+	padding: 0.2cm;
+	user-select: text;
+`;
 
 export const InlineClock = () => {
 	const [time, setTime] = useState(new Date());
@@ -35,38 +46,32 @@ export const InlineClock = () => {
 	];
 
 	return (
-		<h4 style={{ display: 'inline' }}>
-			{months[time.getMonth()] +
+		<Dropdown
+			text={
+				months[time.getMonth()] +
 				' ' +
 				time.getDate() +
 				' ' +
 				time.toLocaleTimeString([], {
 					hour: '2-digit',
-					minute: '2-digit',
-					second: '2-digit'
-				})}
-		</h4>
-
-		//   <Dropdown text={months[time.getMonth()] + " " + time.getDate() + " " +
-		//     time.toLocaleTimeString([], {
-		//       hour: "2-digit",
-		//       minute: "2-digit",
-		//     })}>
-		//     <MenubarList>
-		//       <h2 style={{ display: "inline" }}>{time.toLocaleTimeString([], {
-		//         hour: "2-digit",
-		//         minute: "2-digit",
-		//         second: "2-digit",
-		//       })}</h2>
-		//       <br />
-		//       <h3 style={{ display: "inline" }}>{days[time.getDay()]}{" "}</h3>
-		//       <br />
-		//       <p>
-		//         {months[time.getMonth()]}{" "}
-		//         {time.getDate()}{" "}
-		//         {time.getFullYear()}
-		//       </p>
-		//     </MenubarList>
-		//   </Dropdown>
+					minute: '2-digit'
+				})
+			}>
+			<ExpandedView>
+				<h2 style={{ display: 'inline' }}>
+					{time.toLocaleTimeString([], {
+						hour: '2-digit',
+						minute: '2-digit',
+						second: '2-digit'
+					})}
+				</h2>
+				<br />
+				<h3 style={{ display: 'inline' }}>{days[time.getDay()]} </h3>
+				<br />
+				<p>
+					{months[time.getMonth()]} {time.getDate()} {time.getFullYear()}
+				</p>
+			</ExpandedView>
+		</Dropdown>
 	);
 };
