ffbf07886aa4a0f0344c38990157c6f85b6ef219
Jesper <contact@vecopotryx.com>
2025-04-02 14:43:23 +0200

    Add ability to add back removed apps

--- src/Applications/Settings/ApplicationsSettings.tsx
+++ src/Applications/Settings/ApplicationsSettings.tsx
@@ -1,8 +1,10 @@
 import { useAppManagerStore } from "../AppManagerStore";
+import { defaultApps } from "../DefaultApps";
 import styles from "./Settings.module.css";
 
 export const ApplicationsSettings = () => {
 	const list = useAppManagerStore((state) => state.apps);
+	const removedDefaults = defaultApps.filter((obj) => !list.includes(obj));
 	const addApp = useAppManagerStore((state) => state.addApp);
 	const removeApp = useAppManagerStore((state) => state.removeApp);
 
@@ -35,6 +37,37 @@ export const ApplicationsSettings = () => {
 					))}
 				</tbody>
 			</table>
+			{removedDefaults.length > 0 ? (
+				<>
+					Removed defaults:
+					<table>
+						<tbody>
+							{removedDefaults.map((app) => (
+								<tr key={app.title}>
+									<td
+										style={{
+											display: "flex",
+											alignItems: "center",
+											gap: "0.25em",
+										}}
+									>
+										<img src={app.icon} alt="" style={{ height: "1em" }} />
+										{app.title}
+									</td>
+									<td>{app.type === "component" ? "Built-in" : "External"}</td>
+									<td>
+										<button onClick={() => addApp(app)} type="button">
+											Add
+										</button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</>
+			) : (
+				""
+			)}
 		</div>
 	);
 };
