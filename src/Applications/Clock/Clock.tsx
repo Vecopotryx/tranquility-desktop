f5b60024ae84e82affde7c95c10ec9d4905bb01a
Jesper <contact@vecopotryx.com>
2025-04-02 15:09:59 +0200

    Add basic clock application

---
+++ src/Applications/Clock/Clock.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import type { Application } from "../AppManagerStore";
+import icon from "../../assets/img/icons/clock.svg";
+
+const Clock = () => {
+	const [time, setTime] = useState(new Date());
+
+	useEffect(() => {
+		setInterval(() => {
+			setTime(new Date());
+		}, 1000);
+	}, []);
+
+	const days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	const months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+
+	// TODO: Add stopwatch and stuff
+
+	return (
+		<div
+			style={{
+				left: "0",
+				margin: "auto",
+				right: "0",
+				position: "absolute",
+				textAlign: "center",
+			}}
+		>
+			<h1>
+				{time.toLocaleTimeString([], {
+					hour: "2-digit",
+					minute: "2-digit",
+					second: "2-digit",
+				})}
+			</h1>
+			<p>
+				{days[time.getDay()]} {months[time.getMonth()]} {time.getDate()}{" "}
+				{time.getFullYear()}
+			</p>
+		</div>
+	);
+};
+
+export const ClockApp: Application = {
+	title: "Clock",
+	icon: icon,
+	type: "component",
+	component: <Clock />,
+};
